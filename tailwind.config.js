99c2acb8aaec1c190ee198f1981d14f5c01fc907
Kiran Bobade <kiran.ambadas.bobade@assurant.com>
2021-08-20 16:19:02 +0530

    installed twilwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,26 @@
+const colors = require('tailwindcss/colors');
 module.exports = {
   // mode: 'jit',
   purge: ['./src/**/*.{html,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      white: colors.white,
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+      sky: colors.sky,
+      cyan: colors.cyan
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [require('@tailwindcss/aspect-ratio')
-,require('@tailwindcss/forms')
-,require('@tailwindcss/line-clamp')
-,require('@tailwindcss/typography')
-],
+    , require('@tailwindcss/forms')
+    , require('@tailwindcss/line-clamp')
+    , require('@tailwindcss/typography')
+  ],
 };
